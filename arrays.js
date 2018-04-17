a534119c98903e50ee6cd9f09f74b16d5e4031f1
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-17 05:11:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,7 @@ function removeElementFromBeginningOfArray(array){
 function destructivelyRemoveElementFromEndOfArray(array){
   return array.pop()
 }
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
