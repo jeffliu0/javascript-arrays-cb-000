6eb47e786b973bd1c1388c6383d13751a993e88a
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-17 04:40:20 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToEndOfArray(array, elem){
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, elem){
+
+}
